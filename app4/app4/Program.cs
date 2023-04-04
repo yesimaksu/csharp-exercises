746c1d0a310243c95a616a6ff510b1afb936189a
Yeşim AKSU <ysm99aksu@gmail.com>
2023-04-04 21:39:33 +0300

    app4

---
+++ app4/app4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin;
+            int sayac, enuzunmetin;
+            string[] dizi = new string[10];
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Metni girin :");
+                metin = Console.ReadLine();
+                dizi[i] = metin;
+
+            }
+
+
+            enuzunmetin = dizi[0].Length;
+            sayac = 0;
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (i + 1 < dizi.Length && enuzunmetin < dizi[i + 1].Length)
+                {
+                    enuzunmetin = dizi[i + 1].Length;
+                    sayac = i + 1;
+                }
+            }
+            Console.WriteLine("En uzun metin {0} karakterlidir,{1}.indistedir .", enuzunmetin, sayac);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ app4/app4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("app4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("app4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c89072b3-ddd8-4447-a10d-6e6b0c1fff1f")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
